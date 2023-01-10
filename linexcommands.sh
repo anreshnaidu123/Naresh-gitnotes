f87a876b3f3374fb0f9f2125c068228c314d8743
ammin <annareshnaidu143@gmail.com>
2023-01-10 11:55:50 +0530

    text files commit v1.0

---
+++ linexcommands.sh
@@ -0,0 +1,13 @@
+resh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makresh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makupaaliresh naidu naresh ammineni maa
+marrib makupaaliupaali
